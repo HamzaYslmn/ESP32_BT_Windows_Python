e2644f7dd8586614aa4332ac54fdbbf6a1e942f4
HamzaYslmn <hamzayesilmen7@gmail.com>
2024-07-07 21:47:19 +0300

    Go

--- esp32BLE_Python/xKeyboardMode.py
+++ esp32BLE_Python/xKeyboardMode.py
@@ -8,16 +8,19 @@ console = Console()
 
 async def keyboard_listener(ser):
     keyboard_mode = True
-    console.print("[yellow]Entering keyboard listening mode. Type 'exit_keyboard_mode' to return to main menu.[/]")
+    console.print("[yellow]Entering keyboard listening mode. Press 'esc' to exit.[/]")
     
     pressed_keys = set()
     last_sent_time = 0
     send_interval = 0.01  # 10ms interval
 
     def on_key_event(e):
-        nonlocal pressed_keys, last_sent_time
+        nonlocal pressed_keys, last_sent_time, keyboard_mode  # Add keyboard_mode to nonlocal declaration
         if e.event_type == keyboard.KEY_DOWN:
             pressed_keys.add(e.name)
+            if e.name == 'esc':  # Check if the pressed key is 'esc'
+                keyboard_mode = False
+                console.print("[yellow]Exiting keyboard listening mode...[/]")
         elif e.event_type == keyboard.KEY_UP:
             pressed_keys.discard(e.name)
 
@@ -40,9 +43,7 @@ async def keyboard_listener(ser):
 
     while keyboard_mode:
         command = await asyncio.to_thread(input, "")
-        if command == "exit_keyboard_mode":
-            keyboard_mode = False
-        elif command:
+        if command:
             timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S:%f")[:-3]
             console.print(f"[rgb(240,160,255)]{timestamp} - {command}[/]")
             ser.write((command + '\n').encode('utf-8'))
